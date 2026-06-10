d13824342e9eb8768f1359f8bdb26117a3d08783
mwanagenzi <moses.ngure@valuechainfactory.com>
2026-06-10 11:15:17 +0300

    refact (Fixes): Change baseUrl

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
 
-  static const String baseUrl = 'https://quickly-integral-crawdad.ngrok-free.app';
+  static const String baseUrl = 'https://mongrel-stunning-preferably.ngrok-free.app';
 
   static const String tokenKey = 'auth_token';
   static const String userKey = 'auth_user';
